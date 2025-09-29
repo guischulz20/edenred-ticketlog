06908efed8691855d2c71ce12b3fc93eea4b0fb8
guischulz20 <149545055+guischulz20@users.noreply.github.com>
2025-09-29 09:51:21 -0300

    Update jbs.js

--- jbs.js/jbs.js
+++ jbs.js/jbs.js
@@ -38,25 +38,25 @@ function gerarVisualizacao(titulo) {
     }
 
     const campos = [
-        ['Protocolo', form.protocolo.value],
-        ['Assistente', form.assistente.value],
-        ['Cliente', form.cliente.value],
-        ['Placa', form.placa.value],
-        ['Cidade', form.cidade.value],
-        ['Segmento', form.segmento.value],
-        ['Regional', form.regional.value],
-        ['Tipo de Prestador', tipo],
-        [tipo === 'credenciado' ? 'Nome do EC' : 'Prestador Terceiro', nomePrestador],
-        ['Cidade do Prestador', form.cidadePrestador.value],
-        [`Telefone (${tipoTelefone})`, telefoneSelecionado],
-        ['Valor por KM', form.valorKM.value],
-        ['Valor de Saída', form.valorSaida.value],
-        ['Quantidade de KM', form.quantidadeKM.value],
-        ['Valor do Serviço', form.valorServico.value],
-        ['De Acordo?', deAcordo],
-        ['Início do SOS', formatarDataHora(form.dataInicio.value)],
-        ['Fim do SOS', formatarDataHora(form.dataFim.value)],
-        ['Serviço Realizado', form.servicoRealizado.value]
+        ['*Protocolo*', form.protocolo.value],
+        ['*Assistente*', form.assistente.value],
+        ['*Cliente*', form.cliente.value],
+        ['*Placa*', form.placa.value],
+        ['*Cidade*', form.cidade.value],
+        ['*Segmento*', form.segmento.value],
+        ['*Regional*', form.regional.value],
+        ['*Tipo de Prestador*', tipo],
+        [tipo === '*credenciado*' ? '*Nome do EC*' : '*Prestador Terceiro*', nomePrestador],
+        ['*Cidade do Prestador*', form.cidadePrestador.value],
+        [`*Telefone (${tipoTelefone})*`, telefoneSelecionado],
+        ['*Valor por KM*', form.valorKM.value],
+        ['*Valor de Saída*', form.valorSaida.value],
+        ['*Quantidade de KM*', form.quantidadeKM.value],
+        ['*Valor do Serviço*', form.valorServico.value],
+        ['*De Acordo?*', deAcordo],
+        ['*Início do SOS*', formatarDataHora(form.dataInicio.value)],
+        ['*Fim do SOS*', formatarDataHora(form.dataFim.value)],
+        ['*Serviço Realizado*', form.servicoRealizado.value]
     ];
 
     const camposPreenchidos = campos.filter(c => c[1] && c[1].trim() !== '');
@@ -169,3 +169,4 @@ function limparCampos() {
     document.getElementById('campoCredenciado').classList.add('hidden');
     document.getElementById('campoTerceiro').classList.add('hidden');
 }
+
